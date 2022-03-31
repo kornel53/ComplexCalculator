09a35908d7b3780cd7fccad95233471b0094b0ba
Korneliusz Krawczyk <korneliusz_krawczyk@o2.pl>
2022-03-31 19:48:43 +0200

    Add basic operations

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -40,6 +40,7 @@
             this.textBox4 = new System.Windows.Forms.TextBox();
             this.textBox5 = new System.Windows.Forms.TextBox();
             this.textBox6 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // button1
@@ -62,6 +63,7 @@
             this.button2.TabIndex = 1;
             this.button2.Text = "-";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // button3
             // 
@@ -72,6 +74,7 @@
             this.button3.TabIndex = 2;
             this.button3.Text = "*";
             this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // button4
             // 
@@ -82,6 +85,7 @@
             this.button4.TabIndex = 3;
             this.button4.Text = "/";
             this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
             // 
             // button5
             // 
@@ -102,6 +106,7 @@
             this.button6.TabIndex = 5;
             this.button6.Text = "=";
             this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
             // 
             // textBox1
             // 
@@ -140,6 +145,7 @@
             this.textBox5.Location = new System.Drawing.Point(984, 37);
             this.textBox5.Multiline = true;
             this.textBox5.Name = "textBox5";
+            this.textBox5.ReadOnly = true;
             this.textBox5.Size = new System.Drawing.Size(176, 249);
             this.textBox5.TabIndex = 11;
             // 
@@ -148,14 +154,25 @@
             this.textBox6.Location = new System.Drawing.Point(802, 37);
             this.textBox6.Multiline = true;
             this.textBox6.Name = "textBox6";
+            this.textBox6.ReadOnly = true;
             this.textBox6.Size = new System.Drawing.Size(176, 249);
             this.textBox6.TabIndex = 10;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(376, 48);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 28);
+            this.label1.TabIndex = 12;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1196, 565);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.textBox5);
             this.Controls.Add(this.textBox6);
             this.Controls.Add(this.textBox3);
@@ -189,5 +206,6 @@
         private TextBox textBox4;
         private TextBox textBox5;
         private TextBox textBox6;
+        private Label label1;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -2,6 +2,7 @@ namespace ComplexCalculator
 {
     public partial class Form1 : Form
     {
+        short selection = 0;
         public Form1()
         {
             InitializeComponent();
@@ -17,14 +18,93 @@ namespace ComplexCalculator
             
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private void Calculate(short sw)
+        {
+            switch(sw)
+            {
+                case 1:
+                    Sum();
+                    break;
+                case 2:
+                    Substract();
+                    break;
+                case 3:
+                    Multiply();
+                    break;
+                case 4:
+                    Divide();
+                    break;
+
+            }
+        }
+
+        private void Sum()
         {
             double a, b, c, d;
-            (a,b,c,d) = Nums();
+            (a, b, c, d) = Nums();
             double Re = a + c;
             double Im = b + d;
             textBox5.Text = Convert.ToString(Re);
-            textBox6.Text = Convert.ToString(Im); 
+            textBox6.Text = Convert.ToString(Im);
+        }
+
+        private void Substract()
+        {
+            double a, b, c, d;
+            (a, b, c, d) = Nums();
+            double Re = a - c;
+            double Im = b - d;
+            textBox5.Text = Convert.ToString(Re);
+            textBox6.Text = Convert.ToString(Im);
+        }
+
+        private void Multiply()
+        {
+            double a, b, c, d;
+            (a, b, c, d) = Nums();
+            double Re = a * c - b * d;
+            double Im = b * c + a * d;
+            textBox5.Text = Convert.ToString(Re);
+            textBox6.Text = Convert.ToString(Im);
+        }
+
+        private void Divide()
+        {
+            double a, b, c, d;
+            (a, b, c, d) = Nums();
+            double Re = (a * c + b * d) / (c * c + d * d);
+            double Im = (b * c - a * d) / (c * c + d * d);
+            textBox5.Text = Convert.ToString(Re);
+            textBox6.Text = Convert.ToString(Im);
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            Calculate(selection);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            selection = 1;
+            label1.Text = "+";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            selection = 2;
+            label1.Text = "-";
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            selection = 3;
+            label1.Text = "*";
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            selection = 4;
+            label1.Text = "/";
         }
     }
 }
