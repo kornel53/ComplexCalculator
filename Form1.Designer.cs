961ddd2e53472e268d2452025a793545d71c34ca
Korneliusz Krawczyk <korneliusz_krawczyk@o2.pl>
2022-04-08 08:30:12 +0200

    Power and root implementation

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -36,11 +36,12 @@
             this.button6 = new System.Windows.Forms.Button();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.textBox2 = new System.Windows.Forms.TextBox();
-            this.textBox3 = new System.Windows.Forms.TextBox();
             this.textBox4 = new System.Windows.Forms.TextBox();
-            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
             this.textBox6 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
+            this.button7 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
@@ -96,6 +97,7 @@
             this.button5.TabIndex = 4;
             this.button5.Text = "^";
             this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
             // 
             // button6
             // 
@@ -124,39 +126,39 @@
             this.textBox2.Size = new System.Drawing.Size(176, 53);
             this.textBox2.TabIndex = 7;
             // 
-            // textBox3
-            // 
-            this.textBox3.Location = new System.Drawing.Point(591, 37);
-            this.textBox3.Multiline = true;
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(176, 53);
-            this.textBox3.TabIndex = 9;
-            // 
             // textBox4
             // 
-            this.textBox4.Location = new System.Drawing.Point(409, 37);
+            this.textBox4.Location = new System.Drawing.Point(591, 37);
             this.textBox4.Multiline = true;
             this.textBox4.Name = "textBox4";
             this.textBox4.Size = new System.Drawing.Size(176, 53);
-            this.textBox4.TabIndex = 8;
+            this.textBox4.TabIndex = 9;
             // 
-            // textBox5
+            // textBox3
             // 
-            this.textBox5.Location = new System.Drawing.Point(984, 37);
-            this.textBox5.Multiline = true;
-            this.textBox5.Name = "textBox5";
-            this.textBox5.ReadOnly = true;
-            this.textBox5.Size = new System.Drawing.Size(176, 249);
-            this.textBox5.TabIndex = 11;
+            this.textBox3.Location = new System.Drawing.Point(409, 37);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(176, 53);
+            this.textBox3.TabIndex = 8;
             // 
             // textBox6
             // 
-            this.textBox6.Location = new System.Drawing.Point(802, 37);
+            this.textBox6.Location = new System.Drawing.Point(984, 37);
             this.textBox6.Multiline = true;
             this.textBox6.Name = "textBox6";
             this.textBox6.ReadOnly = true;
             this.textBox6.Size = new System.Drawing.Size(176, 249);
-            this.textBox6.TabIndex = 10;
+            this.textBox6.TabIndex = 11;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(802, 37);
+            this.textBox5.Multiline = true;
+            this.textBox5.Name = "textBox5";
+            this.textBox5.ReadOnly = true;
+            this.textBox5.Size = new System.Drawing.Size(176, 249);
+            this.textBox5.TabIndex = 10;
             // 
             // label1
             // 
@@ -167,16 +169,28 @@
             this.label1.Size = new System.Drawing.Size(0, 28);
             this.label1.TabIndex = 12;
             // 
+            // button7
+            // 
+            this.button7.Font = new System.Drawing.Font("Segoe UI", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button7.Location = new System.Drawing.Point(331, 470);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(176, 64);
+            this.button7.TabIndex = 13;
+            this.button7.Text = "√";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1196, 565);
+            this.Controls.Add(this.button7);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.textBox5);
             this.Controls.Add(this.textBox6);
-            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox5);
             this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button6);
@@ -202,10 +216,11 @@
         private Button button6;
         private TextBox textBox1;
         private TextBox textBox2;
-        private TextBox textBox3;
         private TextBox textBox4;
-        private TextBox textBox5;
+        private TextBox textBox3;
         private TextBox textBox6;
+        private TextBox textBox5;
         private Label label1;
+        private Button button7;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -1,4 +1,4 @@
-namespace ComplexCalculator
+﻿namespace ComplexCalculator
 {
     public partial class Form1 : Form
     {
@@ -34,6 +34,12 @@ namespace ComplexCalculator
                 case 4:
                     Divide();
                     break;
+                case 5:
+                    Power();
+                    break;
+                case 6:
+                    Root();
+                    break;
 
             }
         }
@@ -44,8 +50,8 @@ namespace ComplexCalculator
             (a, b, c, d) = Nums();
             double Re = a + c;
             double Im = b + d;
-            textBox5.Text = Convert.ToString(Re);
-            textBox6.Text = Convert.ToString(Im);
+            textBox6.Text = Convert.ToString(Re);
+            textBox5.Text = Convert.ToString(Im);
         }
 
         private void Substract()
@@ -54,8 +60,8 @@ namespace ComplexCalculator
             (a, b, c, d) = Nums();
             double Re = a - c;
             double Im = b - d;
-            textBox5.Text = Convert.ToString(Re);
-            textBox6.Text = Convert.ToString(Im);
+            textBox6.Text = Convert.ToString(Re);
+            textBox5.Text = Convert.ToString(Im);
         }
 
         private void Multiply()
@@ -64,8 +70,8 @@ namespace ComplexCalculator
             (a, b, c, d) = Nums();
             double Re = a * c - b * d;
             double Im = b * c + a * d;
-            textBox5.Text = Convert.ToString(Re);
-            textBox6.Text = Convert.ToString(Im);
+            textBox6.Text = Convert.ToString(Re);
+            textBox5.Text = Convert.ToString(Im);
         }
 
         private void Divide()
@@ -74,8 +80,91 @@ namespace ComplexCalculator
             (a, b, c, d) = Nums();
             double Re = (a * c + b * d) / (c * c + d * d);
             double Im = (b * c - a * d) / (c * c + d * d);
-            textBox5.Text = Convert.ToString(Re);
-            textBox6.Text = Convert.ToString(Im);
+            textBox6.Text = Convert.ToString(Re);
+            textBox5.Text = Convert.ToString(Im);
+        }
+
+        private (double, double) ToTrig()
+        {
+            double Re = Convert.ToDouble(textBox1.Text);
+            double Im = Convert.ToDouble(textBox2.Text);
+            double Abs = Math.Sqrt(Math.Pow(Re, 2) + Math.Pow(Im, 2));
+            double arg_cos = Re / Abs;
+            double arg_sin = Im / Abs;
+            double asin = Math.Asin(arg_sin);
+            asin = asin * 180 / Math.PI;
+            if(arg_sin > 0 && arg_cos < 0)
+            {
+                asin += 90;
+            }
+            else if(arg_sin <= 0 && arg_cos < 0)
+            {
+                asin = -asin;
+                asin += 180;
+            }
+            else if(arg_sin < 0 && arg_cos >= 0)
+            {
+                asin += 360;
+            }
+
+            double Arg = asin;
+            return (Abs, Arg);
+        }
+
+        private (double, double) ToCanon(double Abs, double Arg)
+        {
+            double pi_Arg = (Arg / 180) * Math.PI;
+            double cos_v = Math.Cos(pi_Arg);
+            double sin_v = Math.Sin(pi_Arg);
+            double Real = Abs * cos_v;
+            double Imag = Abs * sin_v;
+            return (Real, Imag);
+        }
+
+        private void Power()
+        {
+            double z_abs, z_arg;
+            (z_abs, z_arg) = ToTrig();
+            double pow = Convert.ToDouble(textBox3.Text);
+            z_abs = Math.Pow(z_abs, pow);
+            z_arg *= pow;
+            z_arg %= 360;
+            double a, b;
+            (a, b) = ToCanon(z_abs, z_arg);
+            textBox5.Text = Convert.ToString(Math.Round(a, 2));
+            textBox6.Text = Convert.ToString(Math.Round(b, 2));
+        }
+
+        private void Root()
+        {
+            double z_abs, z_arg;
+            (z_abs, z_arg) = ToTrig();
+
+            double root_deg = Convert.ToDouble(textBox3.Text);
+            double root_abs = Math.Pow(z_abs, 1.0 / root_deg);
+            uint udeg = Convert.ToUInt32(root_deg);
+            double[] rtArg = new double[udeg];
+            double[] rtA = new double[udeg];
+            double[] rtB = new double[udeg];
+
+            for (uint i = 0; i < udeg; i++)
+            {
+                rtArg[i] = (z_arg + 360 * i) / root_deg;
+            }
+            for (uint i = 0; i < udeg; i++)
+            {
+                (rtA[i], rtB[i]) = ToCanon(z_abs, rtArg[i]);
+            }
+
+            textBox5.Clear();
+            textBox6.Clear();
+            for (uint i = 0; i < udeg; i++)
+            {
+                textBox5.AppendText(Convert.ToString(Math.Round(rtA[i], 2)));
+                textBox5.AppendText(Environment.NewLine);
+                textBox6.AppendText(Convert.ToString(Math.Round(rtB[i], 2)));
+                textBox6.AppendText(Environment.NewLine);
+            }
         }
 
         private void button6_Click(object sender, EventArgs e)
@@ -105,6 +194,18 @@ namespace ComplexCalculator
         {
             selection = 4;
             label1.Text = "/";
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            selection = 5;
+            label1.Text = "^";
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            selection = 6;
+            label1.Text = "\u221A";
         }
     }
 }
